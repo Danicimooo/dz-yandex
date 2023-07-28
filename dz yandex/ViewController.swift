dc3fe08ae71d25dd3c987260a0e775a31755cd7e
Daniil Myachin <myachin_daniil@mail.ru>
2023-07-28 14:20:20 +0600

    Initial Commit

---
+++ dz yandex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dz yandex
+//
+//  Created by Мячин Даниил on 28.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
